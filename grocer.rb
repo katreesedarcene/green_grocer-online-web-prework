bd3d1e44f51fc52adc8909ef3146e3e018545703
Kat B <katreesebarnes@gmail.com>
2019-03-06 22:30:40 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,45 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+  
+  cart.each_with_object({}) do |item, hash|
+    item.each do |name, attributes|
+     if hash[name]
+       attributes[:count] += 1 
+     else 
+       attributes[:count] = 1
+       hash[name] = attributes
+     end
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    
+      if cart[name] && cart[name][:count] == coupon[:num]
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        elsif
+           cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+           else
+             cart[name][:count] > coupon[:num]
+        end
+        cart[name][:count] -= coupon[:num]
+      end  
+    end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |name, items|
+    if items[:clearance] == true
+      updated_price = items[:price] * 0.80
+      items[:price] = updated_price.round(2)
+    end
+  end 
+  cart
 end
 
 def checkout(cart, coupons)
