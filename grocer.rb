784a9c8024f1391901e3eb812d8fdfe4f8b85052
Kat B <katreesebarnes@gmail.com>
2019-03-17 12:39:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     name = coupon[:item]
     
-      if cart[name] && cart[name][:count] == coupon[:num]
+      if cart[name] && cart[name][:count] >= coupon[:num]
         if cart["#{name} W/COUPON"] 
           cart["#{name} W/COUPON"][:count] += 1 
         elsif
@@ -43,5 +43,5 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidate_cart = consolidate_cart(cart)
 end
